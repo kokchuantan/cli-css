bf64b8d4e80d0285a046e4fa5f1a0d4050d11e94
kokchuantan <kokchuan1996@gmail.com>
2020-04-02 19:16:00 +0800

    moved variable declaration to prevent error

--- index.js
+++ index.js
@@ -7,21 +7,16 @@ var userInput3 = process.argv[5]
 
 const rgbToHslValue = functions.rgbToHsl((parseInt(userInput1)),(parseInt(userInput2)),(parseInt(userInput3)));
 const hexToRgbValue = functions.hexToRgb(userInput1);
-const hexToHslValue = functions.rgbToHsl((hexToRgbValue.r),(hexToRgbValue.g),(hexToRgbValue.b))
+const rgbToHexValue = functions.rgbToHex((parseInt(userInput1)),(parseInt(userInput2)),(parseInt(userInput3)))
 
-var r = 255;
-var b = 0;
-var g = 0;
-
-// this special console will create colored output
-console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, myOutput);
 
 if (userInput == 'hex') {
+    const hexToHslValue = functions.rgbToHsl((hexToRgbValue.r),(hexToRgbValue.g),(hexToRgbValue.b))
     console.log(`rgb( ${hexToRgbValue.r} ${hexToRgbValue.g} ${hexToRgbValue.b} )`);
     console.log(`${hexToHslValue.h} ${hexToHslValue.s} ${hexToHslValue.l}`);
 
 } else if (userInput == 'rgb') {
-    console.log(`${functions.rgbToHex((parseInt(userInput1)),(parseInt(userInput2)),(parseInt(userInput3)))}`)
+    console.log(rgbToHexValue)
     console.log(`${rgbToHslValue.h} ${rgbToHslValue.s} ${rgbToHslValue.l}`);
 }
 
